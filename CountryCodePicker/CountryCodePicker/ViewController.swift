5e5c0039d68685f93ffed98853cfdb4f8b852466
Raja Reddy <rajareddy2606@gmail.com>
2018-05-22 00:05:24 +0530

    Initial Commit

---
+++ CountryCodePicker/CountryCodePicker/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CountryCodePicker
+//
+//  Created by Raja on 21/05/18.
+//  Copyright © 2018 Raja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, CountryCodesDelegate {
+    @IBOutlet weak var statusLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func pickCountryCodeBthHandler(_ sender: Any) {
+        let vc = CountryCodesViewController();
+        vc.delegate = self
+        self.present(vc, animated: true, completion: nil);
+    }
+    
+    
+    func didSelectCountryCode(_ countryName: String, dialingCode: String) {
+        self.statusLbl.text = dialingCode+"\n"+countryName
+    }
+}
+
